95ecf6821f48f3d266e97a84a116030ae12ac08f
mein0014 <42909585+mein0014@users.noreply.github.com>
2018-10-11 04:59:00 +1030

    displaying xml in sortable table

--- proto-pages/reporter.php
+++ proto-pages/reporter.php
@@ -13,62 +13,83 @@ $pageTitle = 'Reporter';
   </div>
   <!-- Main Content -->
   <div class="container">
+    <div class="row">
+      <div class="col-sm-7">
+              <!-- Reporting Form -->
+              <form id="compatabilityForm" method="post">
+                <!-- Title -->
+                <div class="form-group">
+                  <label for="softwareTitle">Software Title</label>
+                  <input id="softwareTitle" class="form-control" type="text" placeholder="e.g. Boulder Dash" required>
+                </div>
 
-    <!-- Reporting Form -->
-    <form id="compatabilityForm">
-      <div class="form-group">
-        <label for="softwareTitle">Software Title</label>
-        <input id="softwareTitle" class="form-control" type="text" placeholder="e.g. Boulder Dash" required>
-      </div>
+                <!-- Dev, Genre, Year -->
+                <div class="form-group">
+                  <label for="softwareDeveloper">Developer</label>
+                  <input id="softwareDeveloper" class="form-control" type="text" placeholder="e.g. Peter Liepa" required>
+                  <label for="softwareGenre">Genre</label>
+                  <input id="softwareGenre" class="form-control" type="text" placeholder="e.g. Puzzle" required>
+                  <label for="softwareYear">Release Year</label>
+                  <input id="softwareYear" class="form-control" type="number" min="1976" placeholder="e.g. 1984" required>
+                </div>
 
-      <div class="form-group">
-        <label for="softwareDeveloper">Developer</label>
-        <input id="softwareDeveloper" class="form-control" type="text" placeholder="e.g. Peter Liepa" required>
-        <label for="softwareGenre">Genre</label>
-        <input id="softwareGenre" class="form-control" type="text" placeholder="e.g. Puzzle" required>
-        <label for="softwareYear">Release Year</label>
-        <input id="softwareYear" class="form-control" type="text" placeholder="e.g. 1984" required>
-      </div>
+                <!-- Compatability Rating -->
+                <div class="form-group">
+                  <label for="softwareCompatability">Compatability Rating</label>
+                  <select id="softwareCompatability" class="form-control" required>
+                    <option>Untested</option>
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                  </select>
+                </div>
 
-      <div class="form-group">
-        <label for="softwareCompatability">Compatability Rating</label>
-        <select id="softwareCompatability" class="form-control" required>
-          <option>Untested</option>
-          <option>1</option>
-          <option>2</option>
-          <option>3</option>
-          <option>4</option>
-          <option>5</option>
-        </select>
-      </div>
-      <div class="form-group">
-        <label for="softwareIssues">Issues Noticed</label>
-        <textarea id="softwareIssues" class="form-control" rows="3"></textarea>
-      </div>
-      <div class="form-group">
-        <div class="form-check">
-          <input id="invalidCheck" class="form-check-input" type="checkbox" value="true" required>
-          <label class="form-check-label" for="invalidCheck">
-            I'm sure I want to generate a report
-          </label>
-          <div class="invalid-feedback">
-            You must agree before submitting.
-          </div>
+                <!-- Issues -->
+                <div class="form-group">
+                  <label for="softwareIssues">Issues Noticed</label>
+                  <textarea id="softwareIssues" class="form-control" rows="3" placeholder="e.g. Stuttering, frame tearing, etc."></textarea>
+                </div>
+
+                <!-- Submission Buttons -->
+                <div class="form-group">
+                  <div class="form-check">
+                    <input id="invalidCheck" class="form-check-input" type="checkbox" value="true" required>
+                    <label class="form-check-label" for="invalidCheck">
+                        I'm sure I want to generate a report
+                    </label>
+                    <div class="invalid-feedback">
+                      You must agree before submitting.
+                    </div>
+                  </div>
+                </div>
+                <button id="btnSave" type="button" class="btn btn-primary" disabled>Generate Report</button>
+              </form>
+              <!-- Reporting Form End -->
+      </div><!-- col end -->
+      <!-- XML DISPLAY -->
+      <div class ="col-sm-5">
+        <div class="form-group">
+          <label for="xmlOutput">XML output</label>
+          <textarea id="xmlOutput" class="form-control" rows="15" disabled></textarea>
         </div>
-      </div>
-      <button id="btnSave" type="button" class="btn btn-primary" disabled>Generate Report</button>
-    </form>
-  </div>
+          <!-- <button type="button" id="filenamer" class="btn btn-secondary btn-sm">Sugest fileName</button> -->
+          <!-- <button type="button" onclick="buildXMLStr()" class="btn btn-secondary btn-sm">make string</button> -->
+          <button type="button" id="btnCopyXML" onclick="copyText('xmlOutput')" class="btn btn-info btn-sm float-right">Copy XML</button>
+      </div><!-- col end -->
+
+    </div><!-- row end -->
+  </div> <!-- container end -->
 
   <script>
+  // Mini Validations
   $("#invalidCheck").click( function() {
-
     if(!$(this).is(':checked')){
       $('#btnSave').attr("disabled","disabled");
     } else
       $('#btnSave').removeAttr('disabled');
   });
-
 
   //Build an XML File from the input
   $("#btnSave").click( function() {
@@ -77,19 +98,7 @@ $pageTitle = 'Reporter';
     filename += ".xml";
     console.log("suggested filename: " + filename)
   });
-  </script>
-<div class="container">
-  <button type="button" id="filenamer" class="btn btn-secondary btn-sm">Sugest fileName</button>
-  <button type="button" onclick="buildXMLStr()" class="btn btn-secondary btn-sm">make string</button>
 
-  <div class="form-group">
-    <label for="xmlOutput">XML output</label>
-    <textarea id="xmlOutput" class="form-control" rows="8" disabled></textarea>
-  </div>
-</div>
-
-
-<script>
   //Disable submit button until checkbox confirmed
   $("#invalidCheck").click( function() {
     if(!$(this).is(':checked')){
@@ -117,20 +126,27 @@ $pageTitle = 'Reporter';
   //Build the XML string
   function buildXMLStr() {
     var fileContent = '';
-    fileContent += "\t<GAME>\n"
-    fileContent += "\t\t<TITLE>" + $("#softwareTitle").val() + "</TITLE>\n";
-    fileContent += "\t\t<DEVELOPER>" + $("#softwareDeveloper").val() + "</DEVELOPER>\n";
-    fileContent += "\t\t<GENRE>" + $("#softwareGenre").val() + "</GENRE>\n";
-    fileContent += "\t\t<YEAR>" + $("#softwareYear").val() + "</YEAR>\n";
-    fileContent += "\t\t<COMPATABILITY>" + $("#softwareCompatability").val() + "</COMPATABILITY>\n";
-    fileContent += "\t\t<ISSUES>" + $("#softwareIssues").val() + "</ISSUES>\n";
-    fileContent += "\t</GAME>\n"
+    fileContent += "<GAME>\n"
+    fileContent += "  <TITLE>" + $("#softwareTitle").val() + "</TITLE>\n";
+    fileContent += "  <DEVELOPER>" + $("#softwareDeveloper").val() + "</DEVELOPER>\n";
+    fileContent += "  <GENRE>" + $("#softwareGenre").val() + "</GENRE>\n";
+    fileContent += "  <YEAR>" + $("#softwareYear").val() + "</YEAR>\n";
+    fileContent += "  <COMPATABILITY>" + $("#softwareCompatability").val() + "</COMPATABILITY>\n";
+    fileContent += "  <ISSUES>" + $("#softwareIssues").val() + "</ISSUES>\n";
+    fileContent += "</GAME>\n"
     console.log(fileContent)
     return String(fileContent);
   }
 
-</script>
 
+  function copyText(ID) {
+    var str = document.getElementById(ID);
+    str.select();
+    document.execCommand("copy");
+    alert("COPIED TEXT:\n" + str.value);
+  }
+
+</script>
 
 </body>
 </html>
---
+++ proto-pages/titlesTestedTable.php
@@ -0,0 +1,34 @@
+<?php
+
+  $directory = "./xml/generated/";
+  // Loop through each XML file in specified directory
+  foreach (glob($directory . "*.xml") as $filename) {
+    $xml=simplexml_load_file($filename) or die("Error: Cannot create object");
+    // Loop through each child "GAME" element and extract info
+    // XML elements are CASE SESITIVE!
+    foreach($xml->children() as $childGAME) {
+      echo "        <tr>\n";
+      echo "          <td>" . $childGAME->TITLE . "</td>\n";
+      echo "          <td>" . getCompatabilityStars($childGAME->COMPATIBILITY) . "</td>\n";
+      echo "          <td>" . date("Y-m-d H:i:s", filemtime($filename)) . "</td>\n";
+      echo "        </tr>\n";
+    }//inner loop end
+  }//outer loop end
+
+
+  // Convert XML compatability number into star rating
+  function getCompatabilityStars($inStr) {
+    $starRating = "N/A";
+    switch ($inStr) {
+      case 1:  $starRating = '&#9733;'; break;
+      case 2:  $starRating = '&#9733;&#9733;'; break;
+      case 3:  $starRating = '&#9733;&#9733;&#9733;'; break;
+      case 4:  $starRating = '&#9733;&#9733;&#9733;&#9733;'; break;
+      case 5:  $starRating = '&#9733;&#9733;&#9733;&#9733;&#9733;'; break;
+      default: $starRating = 'Untested';
+    }
+    // Return number of star symbols
+    return $starRating;
+  }
+
+?>
